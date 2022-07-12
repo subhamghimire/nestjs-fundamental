0de3d0cd3b18469481a8a0c8f87d0b58f9d12e13
subhamghimire <subham.ghimire26@gmail.com>
2022-07-12 16:23:06 +0545

    async for root

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,21 +12,23 @@ import * as Joi from '@hapi/joi';
 import appConfig from './config/app.config';
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
     ConfigModule.forRoot({
       load: [appConfig],
     }),
     CatsModule,
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
     CoffeeRatingModule,
   ],
   controllers: [AppController],
