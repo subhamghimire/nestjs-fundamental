e47b997dcf64b7b8c4782e7b7ffefe543e82dbba
subhamghimire <subham.ghimire26@gmail.com>
2022-07-12 16:06:30 +0545

    custom congig file

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
+import appConfig from './config/app.config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        DATABASE_HOST: Joi.required(),
-        DATABASE_PORT: Joi.number().default(5432),
-      }),
+      load: [appConfig],
     }),
     CatsModule,
     CoffeesModule,
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -24,10 +24,7 @@ export class CoffeesService {
     private readonly dataSource: DataSource,
     private readonly configService: ConfigService,
   ) {
-    const databaseHost = this.configService.get<string>(
-      'DATABASE_HOST',
-      'localhost',
-    );
+    const databaseHost = this.configService.get('database.host', 'localhost');
     console.log(databaseHost);
   }
 
