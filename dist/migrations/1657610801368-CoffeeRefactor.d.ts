273dd9e3fa9f58a02d18f9dd6242b941fd15ce23
subhamghimire <subham.ghimire26@gmail.com>
2022-07-13 13:45:57 +0545

    added logic e2e

--- dist/migrations/1657610801368-CoffeeRefactor.d.ts
+++
@@ -1,5 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-export declare class CoffeeRefactor1657610801368 implements MigrationInterface {
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
