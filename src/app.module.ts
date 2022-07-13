491aa7c47c5d670ddd9b3e92657c9ae60e420256
subhamghimire <subham.ghimire26@gmail.com>
2022-07-13 14:06:44 +0545

    setting up mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { CatsModule } from './cats/cats.module';
 import { CoffeesController } from './coffees/coffees.controller';
 import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CatsModule, CoffeesModule],
+  imports: [
+    CatsModule,
+    CoffeesModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/example'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
